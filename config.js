0e586cbea64004072944fbd4cf2ecbb29aefe953
Vabolos <79908586+Vabolos@users.noreply.github.com>
2025-01-02 21:07:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Generator',
+			icon: 'tree',
+			link: 'https://woochanleee.github.io/project-tree-generator/',
 		},
 		{
 			id: '4',
